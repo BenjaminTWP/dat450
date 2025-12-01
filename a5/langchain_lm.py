aac85b8df9b000354c8119d338bb1e3c13a89bd9
benpe <benpe@minerva.cse.chalmers.se>
2025-12-01 13:37:13 +0100

    Step 2

---
+++ a5/langchain_lm.py
@@ -0,0 +1,25 @@
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain_core.prompts import PromptTemplate
+
+
+def create_pipeline(model_id):
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline("text-generation", model=model,
+                    device=0, tokenizer=tokenizer,
+                    return_full_text=False, max_new_tokens=512)
+
+    hf = HuggingFacePipeline(pipeline=pipe)
+    return hf
+
+def prompt(question, hf):
+    template = """Question: {question}"""
+    prompt = PromptTemplate.from_template(template)
+
+    chain = prompt | hf
+
+    print("\nThe question: \n", question)
+
+    for chunk in chain.stream({"question": question}):
+        print(chunk, end="", flush=True)
--- a5/main.py
+++ a5/main.py
@@ -1,3 +1,4 @@
+from langchain_lm import create_pipeline, prompt
 from data import prepare_data
 
 def section(title):
@@ -6,3 +7,8 @@ def section(title):
 if __name__ == "__main__": 
     section("Step 1: Get the dataset")
     documents, question = prepare_data()
+
+    section("Step 2: Configure your LangChain LM")
+
+    hf_pipeline = create_pipeline("Qwen/Qwen2.5-0.5B-Instruct")
+    prompt("What is electroencephalography?", hf_pipeline)
