e9f0a4ae77f7bb7cfb6595d1d93f774b3c7b0bf8
carlodq <carlodq@minerva.cse.chalmers.se>
2025-12-03 17:53:16 +0100

    kinda works now

--- a5/main.py
+++ a5/main.py
@@ -11,8 +11,18 @@ from transformers import (
 
 from langchain_community.llms import HuggingFacePipeline
 from huggingface_hub import login
+from langchain_core.prompts import PromptTemplate
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from uuid import uuid4
+from typing import Any
+from langchain_core.documents import Document
+from langchain.agents.middleware import AgentMiddleware, AgentState
+from langchain.agents import create_agent
 
-login()
+
+login(token="YOUR TOKEN")
 
 
 def get_docs_quest():
@@ -36,12 +46,12 @@ documents, questions = get_docs_quest()
 
 
 hf = HuggingFacePipeline.from_model_id(
-    model_id="meta-llama/Llama-3.2-1B",
+    model_id="meta-llama/Llama-3.2-1B-Instruct",
     task="text-generation",
-    pipeline_kwargs={"max_new_tokens": 10},
+    pipeline_kwargs={"max_new_tokens": 1},
 )
 
-from langchain_core.prompts import PromptTemplate
+
 
 template = """Question: {question}
 
@@ -53,5 +63,90 @@ chain = prompt | hf
 question = "What is electroencephalography?"
 
 print(chain.invoke({"question": question}))
+
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+text = "This is a test document."
+
+query_result = embeddings.embed_query(text)
+
+print(query_result[:3])
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=80,
+    length_function=len,
+    is_separator_regex=False,
+)
+
+metadatas = [{"id": idx} for idx in documents.index]
+texts = text_splitter.create_documents(texts=documents.abstract.tolist(), metadatas=metadatas)
+
+# for text in texts[:10]:
+#     print(text)
+
+# doc_result = embeddings.embed_documents([text])
+
+
+vector_store = Chroma(
+    collection_name="example_collection",
+    embedding_function=embeddings,
+)
+
+uuids = [str(uuid4()) for _ in range(len(texts))]
+
+print(uuids[:10])
+
+vector_store.add_documents(documents=texts, ids=uuids)
+
+results = vector_store.similarity_search_with_score(
+    "What is programmed cell death?", k=3
+)
+for res, score in results:
+    print(f"* [SIM={score:3f}] {res.page_content} [{res.metadata}]")
+
+
+print("_____________RAG_PIPELINE_____________")
+
+
+class State(AgentState):
+    context: list[Document]
+
+
+class RetrieveDocumentsMiddleware(AgentMiddleware[State]):
+    state_schema = State
+
+    def __init__(self, vector_store):
+        self.vector_store = vector_store
+
+    def before_model(self, state: AgentState) -> dict[str, Any] | None:
+        last_message = state["messages"][-1] # get the user input query
+        retrieved_docs = self.vector_store.similarity_search(last_message.text)  # search for documents
+
+        docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)  
+
+        augmented_message_content = (
+            "You are an expert assistant. Use the following context in your response:"
+            f"\n\n{docs_content}"
+            "\nAwnser the question by writing Awnser=Yes if the question is true and write Awnser=No if the question is false."
+            "\nNow awnser the question, Answer="
+        )
+        return {
+            "messages": [last_message.model_copy(update={"content": augmented_message_content})],
+            "context": retrieved_docs,
+        }
+
+
+model = hf
+agent = create_agent(model, tools=[], middleware=[RetrieveDocumentsMiddleware(vector_store)])
+
+your_query = "Can people die due to cancer?"
+
+for step in agent.stream(
+    {"messages": [{"role": "user", "content": your_query}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
 
 
--- a5/run.sh
+++ a5/run.sh
@@ -4,13 +4,10 @@
 #SBATCH --gres=gpu:L40s:2 --exclude=callisto
 #SBATCH -J DAT450_Assignment_2
 #SBATCH -t 00:30:00
+#SBATCH -o output.txt
+#SBATCH --error=output.err
 
 source /data/courses/2025_dat450_dit247/venvs/dat450_venv/bin/activate
-alpha="$1"
-beta="$2"
-num_topics="$3"
 
-out_file="alpha_${alpha}_beta_${beta}_topics_${num_topics}.txt"
-exec > "$out_file" 2>&1
 
 python3 main.py "$@"  
