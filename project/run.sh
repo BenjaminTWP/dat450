0c8d1b0bbed0dbaeb597c1daea90212d08831c4a
erngard <erngard@minerva.cse.chalmers.se>
2025-12-21 21:16:50 +0100

    Update run folder output

--- project/run.sh
+++ project/run.sh
@@ -2,7 +2,9 @@
 #SBATCH -c 8
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:L40s:1
-#SBATCH -J DAT450_Assignment_2
+#SBATCH -J MT
+#SBATCH --output=runs/%x_jobId_%j.out
+#SBATCH --error=runs/%x_jobId_%j.err
 
 source bravenewvenv/bin/activate
 
